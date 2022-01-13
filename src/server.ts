14eff999eda0e1fe37d735606fae52be70256462
AhmedHassan <ahmed.hassan@3ddx.com>
2022-01-13 04:03:33 +0200

    write code that start server successfully

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,16 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const app = express();
+const app: express.Application = express();
 
 app.use(cors());
 
-app.get('/', (_req, res) => {
+app.get('/', (_req: Request, res: Response) => {
     res.send('Hello World!');
 });
+
 const PORT: string | undefined = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`App running and listening on port ${PORT}!`);
